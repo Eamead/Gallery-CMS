ae1e3c45a02044b799f9045b355a8584e6e1bd12
SC-Elizabeth <56262076+Eamead@users.noreply.github.com>
2024-10-31 16:29:28 -0400

    Define Tag Relationships

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = ['name', 'type'];
+
+    public function resources()
+    {
+        return $this->belongsToMany(Resource::class, 'resource_tags');
+    }
 }
