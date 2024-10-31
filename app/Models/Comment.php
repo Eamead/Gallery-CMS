4a247a168b093e2306f3cbb593f9860d7bce8ca1
SC-Elizabeth <56262076+Eamead@users.noreply.github.com>
2024-10-31 16:26:42 -0400

    Define Comment Relationships

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['photo_id', 'user_id', 'content', 'is_approved', 'is_pinned'];
+    
+    public function resource()
+    {
+        return $this->belongsTo(Resource::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
