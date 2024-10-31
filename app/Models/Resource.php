98656fae4b73e4bd51bd9b8610f760c4db063f69
SC-Elizabeth <56262076+Eamead@users.noreply.github.com>
2024-10-31 16:22:55 -0400

    Define Resource relationships in model

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
-    //
+    protected $fillable = [
+        'type', 'user_id', 'title', 'description', 'file_path', 'upload_date',
+        'estimated_date', 'estimated_location', 'privacy_setting', 'gallery_id',
+        'is_featured', 'additional_info', 'copyright_info', 'url'
+    ];
+
+    // Relationships
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function gallery()
+    {
+        return $this->belongsTo(Gallery::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'resource_categories');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'resource_tags');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
