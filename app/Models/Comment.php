8efcabb171d04c51609d91a2ad479d9044640361
SC-Elizabeth <56262076+Eamead@users.noreply.github.com>
2024-10-31 16:28:14 -0400

    Define Category relationships

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['photo_id', 'user_id', 'content', 'is_approved', 'is_pinned'];
-    
+
     public function resource()
     {
         return $this->belongsTo(Resource::class);
