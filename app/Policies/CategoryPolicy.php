073bca1c0168a96b8aaecb83a0eff6f49f4a3cd8
SC-Elizabeth <56262076+Eamead@users.noreply.github.com>
2024-11-11 13:31:59 -0500

    Create CategoryPolicy.php

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class CategoryPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Category $category): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Category $category): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Category $category): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Category $category): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Category $category): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can manage categories.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+    */
+    public function manage(User $user)
+    {
+        return $user->role === 'admin'
+            ? Response::allow()
+            : Response::deny('You do not have permission to manage categories.');
+    }
+}
